7bc09cd793be16343aef5e594cc3f1802ced42cd
Trexdog319 <nbusedu@gmail.com>
2024-12-01 04:48:30 -0500

    Update qualifications and mockStats with new entries and improved formatting

--- skill-scope-site-2/src/data/mockStats.ts
+++ skill-scope-site-2/src/data/mockStats.ts
@@ -1,24 +1,27 @@
 import { StatsData } from '../types/stats';
- export const mockStats: StatsData = {
- languages: [
-{ name: 'c', value: '100.0' },
-{ name: 'r', value: '100.0' },
-{ name: 'go', value: '100.0' },
-{ name: 'aura', value: '100.0' },
-{ name: 'lua', value: '25.09' },
-],libraries: [{ name: 'of', value: '100.0' },
-{ name: 'a', value: '100.0' },
-{ name: 'our', value: '100.0' },
-{ name: 'the', value: '100.0' },
-{ name: 'we', value: '99.09' },
-],tools: [{ name: 'of', value: '100.0' },
-{ name: 'a', value: '100.0' },
-{ name: 'our', value: '100.0' },
-{ name: 'the', value: '100.0' },
-{ name: 'we', value: '99.09' },
-],softSkills: [{ name: 'management', value: '100.0' },
-{ name: 'our', value: '100.0' },
-{ name: 'communication', value: '100.0' },
-{ name: 'we', value: '99.09' },
-{ name: 'leadership', value: '41.37' },
+export const mockStats: StatsData = {
+languages: [
+{ name: 'python', value: '24.45%' },
+{ name: 'java', value: '15.11%' },
+{ name: 'sql', value: '11.81%' },
+{ name: 'matlab', value: '9.07%' },
+{ name: 'javascript', value: '8.52%' }
+],libraries: [
+{ name: 'requests', value: '50.0%' },
+{ name: 'pytorch', value: '15.38%' },
+{ name: 'django', value: '7.69%' },
+{ name: 'tensorflow', value: '7.69%' },
+{ name: 'opencv', value: '3.85%' }
+],tools: [
+{ name: 'autocad', value: '38.46%' },
+{ name: 'git', value: '9.89%' },
+{ name: 'docker', value: '6.59%' },
+{ name: 'kubernetes', value: '5.49%' },
+{ name: 'vmware', value: '4.4%' }
+],softSkills: [
+{ name: 'communication', value: '21.89%' },
+{ name: 'leadership', value: '13.47%' },
+{ name: 'communication skills', value: '10.26%' },
+{ name: 'innovation', value: '6.98%' },
+{ name: 'planning', value: '6.26%' }
 ],};
