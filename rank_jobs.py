cbc37c647c265f95c13096acc698ef63e82dc577
braden <bradenqkirk@gmail.com>
2024-11-30 20:30:35 -0500

    ok

--- rank_jobs.py
+++ rank_jobs.py
@@ -4,11 +4,23 @@ import re
 
 
 global POSSIBLE_QUALIFICATIONS, JOBS
-POSSIBLE_QUALIFICATIONS = list()
+POSSIBLE_LANGUAGES = list()
 JOBS = list() 
 
-def get_qualifications():
-    with open('qualifications.txt') as f:
+def get_languages():
+    with open('qualifications/languages.txt') as f:
+        return [x.strip() for x in f.readlines()]
+
+def get_libraries():
+    with open('qualifications/libraries.txt') as f:
+        return [x.strip() for x in f.readlines()]
+    
+def get_tools():
+    with open('qualifications/tools.txt') as f:
+        return [x.strip() for x in f.readlines()]
+    
+def get_softskills():
+    with open('qualifications/softskills.txt') as f:
         return [x.strip() for x in f.readlines()]
 
 def get_jobs():
@@ -21,24 +33,28 @@ def get_jobs():
     return [' '.join(job.split()) for job in jobs_]
 
 
-POSSIBLE_QUALIFICATIONS = get_qualifications()
+POSSIBLE_LANGUAGES = get_languages()
+POSSIBLE_LIBRARIES = get_libraries()
+POSSIBLE_TOOLS = get_tools()
+POSSIBLE_SOFTSKILLS = get_softskills()
 JOBS = get_jobs()
 
-def check_new_qualifications(qualifications):
+def check_new_qualifications(qualifications, list_):
     for qualification in qualifications:
-        if qualification not in POSSIBLE_QUALIFICATIONS:
-            POSSIBLE_QUALIFICATIONS.append(qualification)
+        if qualification not in list_:
+            list_.append(qualification)
             with open('qualifications.txt', 'a') as f:
                 f.write(qualification + '\n')
-    POSSIBLE_QUALIFICATIONS.append(qualification)
+    list_.append(qualification)
+    return list_
 
-def job_qualifications_list(job):
+def job_qualifications_list(job, qualifications_list):
     job = set(job.lower().split())
 
     qualifications = set()
-    for i in range(len(POSSIBLE_QUALIFICATIONS)):
-        if POSSIBLE_QUALIFICATIONS[i] in job:
-            qualifications.add(POSSIBLE_QUALIFICATIONS[i])
+    for i in range(len(qualifications_list)):
+        if qualifications_list[i] in job:
+            qualifications.add(qualifications_list[i])
     return qualifications
 
 # basic algorithm to rank jobs based on qualifications # replace with ML model?
@@ -62,14 +78,67 @@ def get_best_jobs(jobs, personal_qualifications):
         ranked_jobs_list.append((jobs[i][:15], round(num, 2)))
     return ranked_jobs_list
 
-def main(personal_qualifications):
-    check_new_qualifications(personal_qualifications)
-    ranked_jobs_list = sorted(get_best_jobs(JOBS, personal_qualifications), key=lambda x: -x[1])
-    print(ranked_jobs_list)
+def get_best(jobs, type):
+    jobs_dict = dict()
+    if type == "languages":
+        skills = POSSIBLE_LANGUAGES
+    elif type == "libraries":
+        skills = POSSIBLE_LIBRARIES
+    elif type == "softkills":
+        skills = POSSIBLE_SOFTSKILLS
+    elif type == "tools":
+        skills = POSSIBLE_TOOLS
+    for job in jobs:
+        job_qualifications = job_qualifications_list(job, skills)
+        for qualification in job_qualifications:
+            if qualification in jobs_dict:
+                jobs_dict[qualification] += 1
+            else:
+                jobs_dict[qualification] = 1
+    return jobs_dict
+
+# def main(personal_qualifications):
+#     check_new_qualifications(personal_qualifications)
+#     ranked_jobs_list = sorted(get_best_jobs(JOBS, personal_qualifications), key=lambda x: -x[1])
+#     print(ranked_jobs_list)
+
+# if __name__ == "__main__":
+#     PERSONAL_QUALIFICATIONS = ["python", "c"]
+#     main(PERSONAL_QUALIFICATIONS)
+
+def percent_of_dict(dictionary, total):
+    for key in dictionary:
+        dictionary[key] = round((dictionary[key] / total) * 100, 2)
+    return dictionary
 
 if __name__ == "__main__":
-    PERSONAL_QUALIFICATIONS = ["python", "c"]
-    main(PERSONAL_QUALIFICATIONS)
+    # # languages
+    # best_languages = get_best(JOBS, "languages")
+    # sorted_best_languages = dict(sorted(best_languages.items(), key=lambda item: item[1], reverse=True))
+    # print(sorted_best_languages)
+    # # percent
+    # print(percent_of_dict(sorted_best_languages, len(JOBS)))
 
+    # # libraries
+    # best_libraries = get_best(JOBS, "libraries")
+    # sorted_best_libraries = dict(sorted(best_libraries.items(), key=lambda item: item[1], reverse=True))
+    # print(sorted_best_libraries)
+    # # percent
+    # print(percent_of_dict(sorted_best_libraries, len(JOBS)))
+
+    # # tools
+    # best_tools = get_best(JOBS, "tools")
+    # sorted_best_tools = dict(sorted(best_tools.items(), key=lambda item: item[1], reverse=True))
+    # print(sorted_best_tools)
+    # # percent
+    # print(percent_of_dict(sorted_best_tools, len(JOBS)))
+
+    # softskills
+    best_softskills = get_best(JOBS, "softkills")
+    sorted_best_softskills = dict(sorted(best_softskills.items(), key=lambda item: item[1], reverse=True))
+    print(sorted_best_softskills)
+    # percent
+    print(percent_of_dict(sorted_best_softskills, len(JOBS)))
+          
 
 
